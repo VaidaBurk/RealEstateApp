56f7530173e2419b26baac2841900cbfe6457c04
VaidaBurk <v.burkauskaite@gmail.com>
2021-06-19 10:00:18 +0300

    Initial commit

---
+++ RealEstateApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RealEstateApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<RealEstate> realEstateObjects = new List<RealEstate>();
+
+            realEstateObjects.Add(new RealEstate("flat", 112000, 64.2, "Vilnius"));
+            realEstateObjects.Add(new RealEstate("flat", 68000, 67.02, "Klaipėda"));
+            realEstateObjects.Add(new RealEstate("flat", 92000, 38, "Svencelė"));
+            realEstateObjects.Add(new RealEstate("house", 66600, 49, "Klaipėda"));
+            realEstateObjects.Add(new RealEstate("house", 149000, 40, "Dreverna"));
+            realEstateObjects.Add(new RealEstate("house", 460000, 150, "Karklė"));
+
+            for (int i = 0; i < realEstateObjects.Count; i++)
+            {
+                Console.WriteLine(realEstateObjects[i]);
+            }
+        }
+    }
+}
---
+++ RealEstateApp/RealEstate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp
+{
+    class RealEstate
+    {
+        public RealEstate(string type, double price, double area, string city)
+        {
+            this.Type = type;
+            this.Price = price;
+            this.Area = area;
+            this.City = city;
+        }
+
+        public string Type { get; set; }
+        public double Price { get; set; }
+        public double Area { get; set; }
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            string realEstateObject = "type: " + this.Type + "; price: " + this.Price + " Eur; area: " + this.Area + " m²; city: " + this.City 
+                + "; price per m²: " + this.sqmPrice() + " Eur";
+            return realEstateObject;
+        }
+
+        public double sqmPrice()
+        {
+            double sqmPrice = (this.Price / this.Area);
+            sqmPrice = Math.Round(sqmPrice, 2);
+            return sqmPrice;
+        }
+    }
+}
